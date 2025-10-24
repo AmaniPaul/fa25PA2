3fd3e330978b80b291f8993571e8b771e580ce4c
Amani <apaul8486@sdsu.edu>
2025-10-23 20:26:33 -0700

    added buildEncodingTree

--- main.cpp
+++ main.cpp
@@ -93,20 +93,29 @@ int buildEncodingTree(int nextFree) {
     // 1. Create a MinHeap object.
     MinHeap heap = MinHeap();
     // 2. Push all leaf node indices into the heap.
-    heap.push(nextFree, weightArr);
+    for (int i = 0; i < nextFree; ++i) {
+        heap.push(i, weightArr);
+    }
     // 3. While the heap size is greater than 1:
     //    - Pop two smallest nodes
     //    - Create a new parent node with combined weight
     //    - Set left/right pointers
     //    - Push new parent index back into the heap
-    while (heap.size() > 1) {
+    while (heap.size>1) {
         int smallOne = heap.pop(weightArr);
         int smallTwo = heap.pop(weightArr);
-        int parent = smallOne + smallTwo;
-        lef
+
+        int parent = nextFree++;
+
+        weightArr[parent] = weightArr[smallOne] + weightArr[smallTwo];
+
+        leftArr[parent] = smallOne;
+        rightArr[parent] = smallTwo;
+
+        heap.push(parent, weightArr);
     }
     // 4. Return the index of the last remaining node (root)
-    return -1; // placeholder
+    return heap.pop(weightArr); // placeholder
 }
 
 // Step 4: Use an STL stack to generate codes
